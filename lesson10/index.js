1191ed5401d6c4c92eec4c78f3de3444ab6e55e7
AyubovAbdulaziz <abdulazizayubov@gmail.com>
2026-04-10 09:45:18 +0500

    while and do while

---
+++ lesson10/index.js
@@ -0,0 +1,134 @@
+// // 1. While
+// // 2. do While
+
+// // FOR loop
+// for (let raqam1 = 1; raqam1 <= 10; raqam1++) {
+//     console.log(raqam1);
+// }
+
+// // WHILE loop
+// let raqam2 = 1
+// while (raqam2 <= 10) {
+//     console.log(raqam2);
+//     raqam2++
+// }
+
+// // DO WHILE loop
+// let raqam3 = 1
+// do {
+//     console.log(raqam3);
+//     raqam3++
+// } while (raqam3 <= 10)
+
+// 1 dan 100 gacha bo‘lgan sonlar yig‘indisini toping.
+// let raqam1 = 1
+// let yigindi = 0
+// while (raqam1 <= 100){
+//     yigindi += raqam1
+//     raqam1++
+// }
+// console.log(yigindi);
+
+// console.log("-----------------------------------------------------------------------");
+
+// let raqam2 = 1
+// let yigindi2 = 0
+// do {
+//     yigindi2 += raqam2
+//     raqam2++
+// } while (raqam2 <= 100)
+// console.log(yigindi2);
+    
+
+// let raqam1 = 1;
+// while (raqam1 <= 50){
+//     if (raqam1 % 2 === 0) console.log(raqam1);
+//     raqam1++
+// }
+
+// console.log("------------------------------------------------------------------");
+
+// let raqam2 = 1;
+// do {
+//     if (raqam2 % 2 === 0) console.log(raqam2);
+//     raqam2++;
+// }while (raqam2 <= 50)
+
+
+// let raqam1 = 1;
+// while (raqam1 <= 50){
+//     if (raqam1 % 2 === 1) console.log(raqam1);
+//     raqam1++
+// }
+
+// console.log("------------------------------------------------------------------");
+
+// let raqam2 = 1;
+// do {
+//     if (raqam2 % 2 === 1) console.log(raqam2);
+//     raqam2++;
+// }while (raqam2 <= 50)
+
+// let n = 15
+// let raqam1 = 1
+// while (raqam1 <= n) {
+//     console.log(raqam1 * raqam1);
+//     raqam1++
+// }
+
+// console.log("------------------------------------------------------------------");
+
+// let a = 15
+// let raqam2 = 1
+// do {
+//     console.log(raqam2 * raqam2);
+//     raqam2++
+// }while (raqam2 <= a)
+
+// let b = 20
+// let raqam1 = 1
+// while (raqam1 <= b) {
+//     if (raqam1 % 3 === 0) console.log(raqam1);
+//     raqam1++
+// }
+
+// console.log("------------------------------------------------------------------");
+
+// let c = 20
+// let raqam2 = 1
+// do {
+//     if (raqam2 % 3 === 0) console.log(raqam2);
+//     raqam2++
+// }while (raqam2 <= c)
+
+// let n= 10
+// let kupaytma = 1 
+// for(let i = 1; i<= n; i++){
+//     kupaytma *=  i
+// }
+// console.log(kupaytma);
+
+let n = 10
+let kupaytma = 1
+let raqam1 = 1
+while (raqam1 <= n) {
+    kupaytma *= raqam1
+    raqam1++
+}
+console.log(kupaytma);
+
+console.log("------------------------------------------------------------------");
+
+let n2 = 10
+let kupaytma2 = 1
+let raqam2 = 1
+do {
+   kupaytma2 *= raqam2
+   raqam2++ 
+} while(raqam2 <= n2)
+console.log(kupaytma2);
+
+
+
+
+
