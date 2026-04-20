eaa0c85d217e29ad54a739fcf899426d6e320dfe
AyubovAbdulaziz <abdulazizayubov@gmail.com>
2026-04-20 09:05:56 +0500

    return

---
+++ lesson13/index.js
@@ -0,0 +1,66 @@
+// Function
+
+// let a = 10
+// let b = 20
+// let c = 30
+
+// function maxSonniTipibBer(bir, ikki, uch){
+//     if (bir > ikki && bir > uch) console.log(bir, "Katta");
+//     else if (ikki > bir && ikki > uch) console.log(ikki, "Katta");
+//     else if (uch > bir && uch > ikki) console.log(uch, "Katta");
+//     else console.log(bir, ikki, "va", uch, "Bular bir-biriga teng");
+// }
+
+// maxSonniTipibBer(10, 20, 30);
+// maxSonniTipibBer(a, b, c)
+
+// let a = 1
+// let b = 2
+// let c = 3
+// function getValue(c, a, b){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// getValue(b, c, a)
+
+// let bir = 5
+// let ikki = 3
+// let uch = 1
+// let turt = 2
+// let besh = 4
+
+// function getData(bir, ikki, uch, turt, besh){
+//     console.log(ikki);
+//     console.log(bir);
+//     console.log(turt);
+//     console.log(uch);
+//     console.log(besh);
+// }
+
+// getData(ikki, besh, bir, turt, uch)
+
+// let a = 2
+// let b = 1
+// let c = 3
+
+// function getResult(b, a, c, d){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+// }
+
+// getResult(a + b, b * c, c / a, a - b + c);
+
+// RETURN - qaytarish, qaytmoq
+
+// let a = 10
+// let b = 20
+// function yigindi(raqam1, raqam2) {
+//     let summa = raqam1 + raqam2;
+//     return summa;
+// }
+
+// console.log(yigindi(a, b));
